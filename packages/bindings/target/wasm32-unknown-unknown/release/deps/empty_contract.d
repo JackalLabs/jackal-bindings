51b50b6b163e94477c10e1fbbebacef02c450b75
BiPhan4 <biphan374@gmail.com>
2023-05-25 14:14:58 +1200

    empty bindings contract builds to wasm. Will convert to package later

---
+++ packages/bindings/target/wasm32-unknown-unknown/release/deps/empty_contract.d
@@ -0,0 +1,5 @@
+/home/bi/jackal/jackal-bindings/packages/bindings/target/wasm32-unknown-unknown/release/deps/empty_contract.wasm: src/lib.rs
+
+/home/bi/jackal/jackal-bindings/packages/bindings/target/wasm32-unknown-unknown/release/deps/empty_contract.d: src/lib.rs
+
+src/lib.rs:
---
+++ packages/bindings/target/wasm32-unknown-unknown/release/empty_contract.d
@@ -0,0 +1,1 @@
+/home/bi/jackal/jackal-bindings/packages/bindings/target/wasm32-unknown-unknown/release/empty_contract.wasm: /home/bi/jackal/jackal-bindings/packages/bindings/src/lib.rs
